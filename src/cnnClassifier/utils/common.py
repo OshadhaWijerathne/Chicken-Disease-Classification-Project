6d4f43573457e5f13352d643861931c3fc79a265
OshadhaWijerathne <oshadha.20@cse.mrt.ac.lk>
2025-07-20 11:07:17 +0530

    user app and prediction added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -75,3 +75,9 @@ def save_json(path: Path, data: dict):
         json.dump(data, f, indent=4)
 
     logger.info(f"json file saved at: {path}")
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
